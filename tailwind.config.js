971eb35e01fab61db40b46328307af18e0dbcb81
冠逸 <yaj55ushydna@gmail.com>
2021-06-05 21:19:45 +0800

    utility test html set rwd、hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
-    enabled: true,
-    content: ['./*.html'],
+    // enabled: true,
+    // content: ['./*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
